06305c4e8ac3ab09e7382465ff45ed7a3d0710b7
mohammadmohammadhasani <if20b301@technikum-wien.at>
2022-12-13 19:40:02 +0100

    implemented HTTP DELETE

--- CMS.Server.Api/Controllers/NotificationController.cs
+++ CMS.Server.Api/Controllers/NotificationController.cs
@@ -46,5 +46,18 @@ namespace CMS.Server.Api
 
             return StatusCode(StatusCodes.Status400BadRequest, JsonConvert.SerializeObject(new ServerResponse(response.Message)));
         }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var response = this._cmsServerOperationHandler.DeleteNotification(id);
+
+            if (response.IsExecuted)
+            {
+                return StatusCode(StatusCodes.Status200OK, JsonConvert.SerializeObject(new ServerResponse(response.Message)));
+            }
+
+            return StatusCode(StatusCodes.Status400BadRequest, JsonConvert.SerializeObject(new ServerResponse(response.Message)));
+        }
     }
 }
